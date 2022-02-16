cab58401e19a640e35a37dfc5bd3e557865ca114
Hez-X <85429304+Hez-X@users.noreply.github.com>
2022-02-16 17:34:22 +0530

    Add files via upload

---
+++ req.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("echo '\n \033[91m REQUIREMENT AVAILABLE' ")
+os.system("echo '\033[91mINSTALLING...' ")
+os.system("pip install speedtest-cli && pip install --upgrade pip")
+os.system("echo '\033[91m PKG INSTALL SUCCESSFULLY...' ")
+os.system("echo '\n\033[94m DONE...!!!' ")
